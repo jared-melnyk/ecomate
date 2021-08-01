83f16c6d6d5ce0eab29d6329837876d17e948946
Jared Melnyk <jaredmelnyk@Jareds-MacBook-Air.local>
2021-08-01 10:58:41 -0500

    initial kermit for chrome extension using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './button';
 
-function App() {
+
+
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="./sprout-logo-small.png" className="App-logo" alt="logo" />
+        <h1>
+          Jared's First Chrome Extension
+        </h1>
+        <Button />
       </header>
+      <script src="./popup.js"></script>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,35 @@
+/* global chrome */
+import { useEffect } from "react";
+
+const Button = () => {
+  useEffect(() => {
+    let changeColor = document.getElementById("changeColor");
+
+    chrome.storage.sync.get("color", ({ color }) => {
+      changeColor.style.backgroundColor = color;
+    });
+    changeColor.addEventListener("click", async () => {
+      let [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+      });
+
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setPageBackgroundColor,
+      });
+    });
+
+    // The body of this function will be executed as a content script inside the
+    // current page
+    function setPageBackgroundColor() {
+      chrome.storage.sync.get("color", ({ color }) => {
+        document.body.style.backgroundColor = color;
+      });
+    }
+    console.log("mount it!");
+  }, []);
+  return <button id="changeColor">Change Background Color</button>;
+};
+
+export default Button;
